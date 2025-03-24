4ac7cdc88ddc41b88b55d34a6e5d9c52a52b937b
Yevhen Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-03-24 16:54:52 +0200

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import { pinoHttp } from 'pino-http';
+import cors from 'cors';
+import { getEnvVar } from './utils/getEnvVar.js';
+
+const PORT = Number(getEnvVar('PORT', '3000'));
+export const startServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pinoHttp({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.json({ message: 'Hello world!' });
+  });
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Page not found',
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    console.dir(`http://localhost:${PORT}`, { color: 'blue' });
+  });
+};
