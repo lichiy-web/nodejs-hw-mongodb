8253dd73c8d650554d7bf843514d9d68f51bee0b
Eugene Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-04-09 13:07:04 +0300

    final

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -2,6 +2,7 @@ import Joi from 'joi';
 import { NAME_MAX_LENGTH, NAME_MIN_LENGTH } from '../constants/index.js';
 
 export const createContactSchema = Joi.object({
+  userId: Joi.string().required(),
   name: Joi.string().min(NAME_MIN_LENGTH).max(NAME_MAX_LENGTH).required(),
   phoneNumber: Joi.string()
     .min(NAME_MIN_LENGTH)
@@ -13,6 +14,7 @@ export const createContactSchema = Joi.object({
 });
 
 export const updateContactSchema = Joi.object({
+  userId: Joi.string().required(),
   name: Joi.string().min(NAME_MIN_LENGTH).max(NAME_MAX_LENGTH),
   phoneNumber: Joi.string().min(NAME_MIN_LENGTH).max(NAME_MAX_LENGTH),
   email: Joi.string().email(),
