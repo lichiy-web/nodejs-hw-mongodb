f57e27de090f4ecf5d88b4378953065eb53ab7c9
Yevhen Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-04-16 15:23:30 +0300

    prepare to upload

--- src/constants/index.js
+++ src/constants/index.js
@@ -24,5 +24,13 @@ export const SMTP = {
   PASSWORD: 'SMTP_PASSWORD',
   FROM: 'SMTP_FROM',
 };
-
 export const TEMPLATES_DIR = path.resolve('src', 'templates');
+
+export const TEMP_UPLOAD_DIR = path.resolve('temp');
+export const UPLOAD_DIR = path.resolve('uploads');
+
+export const CLOUDINARY = {
+  CLOUD_NAME: 'CLOUD_NAME',
+  API_KEY: 'API_KEY',
+  API_SECRET: 'API_SECRET',
+};
--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
+import { UPLOAD_DIR } from './constants/index.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -23,6 +24,8 @@ export const setupServer = () => {
       },
     }),
   );
+
+  app.use('/upload', express.static(UPLOAD_DIR));
 
   app.use(router);
 
