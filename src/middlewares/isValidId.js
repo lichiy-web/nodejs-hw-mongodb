d57911423a76cedf111838d7ad218420738b8639
Yevhen Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-04-22 00:24:36 +0300

    fix isValidId

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -5,9 +5,11 @@ import { RES_MSG } from '../constants/contacts.js';
 export const isValidId = (req, res, next) => {
   const { contactId } = req.params;
   if (!isValidObjectId(contactId)) {
-    throw createHttpError(400, RES_MSG[401].default, {
-      details: `ContactId: ${contactId} is not valid`,
-    });
+    next(
+      createHttpError(400, RES_MSG[400].default, {
+        details: `ContactId: ${contactId} is not valid`,
+      }),
+    );
   }
   next();
 };
